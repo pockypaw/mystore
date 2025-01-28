9c24fd86323ad01217d15f41eadd1b7b96343001
Ardian <ardridhz@gmail.com>
2025-01-28 14:18:57 +0700

    update

--- backend/app.js
+++ backend/app.js
@@ -17,31 +17,31 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Configure Helmet
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "'unsafe-inline'", "https://mystore-mocha.vercel.app"],
-        connectSrc: ["'self'", "https://mystore-mocha.vercel.app"],
-        imgSrc: ["'self'", "data:", "https://mystore-mocha.vercel.app"],
-        styleSrc: ["'self'", "'unsafe-inline'", "https://mystore-mocha.vercel.app"],
-        frameSrc: ["'self'", "https://mystore-mocha.vercel.app"],
-      },
-    },
-    crossOriginEmbedderPolicy: false, // Optional: Disable COEP if needed
-  })
-);
+// // Configure Helmet
+// app.use(
+//   helmet({
+//     contentSecurityPolicy: {
+//       directives: {
+//         defaultSrc: ["'self'"],
+//         scriptSrc: ["'self'", "'unsafe-inline'", "https://mystore-mocha.vercel.app"],
+//         connectSrc: ["'self'", "https://mystore-mocha.vercel.app"],
+//         imgSrc: ["'self'", "data:", "https://mystore-mocha.vercel.app"],
+//         styleSrc: ["'self'", "'unsafe-inline'", "https://mystore-mocha.vercel.app"],
+//         frameSrc: ["'self'", "https://mystore-mocha.vercel.app"],
+//       },
+//     },
+//     crossOriginEmbedderPolicy: false, // Optional: Disable COEP if needed
+//   })
+// );
 
-// Enable CORS
-app.use(
-  cors({
-    origin: "https://mystore-mocha.vercel.app", // Allow requests from this origin
-    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
-    credentials: true, // Allow cookies or authorization headers
-  })
-);
+// // Enable CORS
+// app.use(
+//   cors({
+//     origin: "https://mystore-mocha.vercel.app", // Allow requests from this origin
+//     methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+//     credentials: true, // Allow cookies or authorization headers
+//   })
+// );
 
 // Connect to the database
 (async () => {
