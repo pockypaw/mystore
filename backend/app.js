2f689bf30f94b47d5c676ffd68e45603ba743e32
Ardian <ardridhz@gmail.com>
2025-01-28 17:55:13 +0700

    fix

--- backend/app.js
+++ backend/app.js
@@ -55,16 +55,16 @@ app.use(express.urlencoded({ extended: true }));
 })();
 
 app.use("/api/v1/product", ProductRoutes);
-app.get("/hello", (req, res) => {
-  res.send("hello");
-});
+app.get('/hello',(req,res)=>{
+  res.send('hello');
+})
 
 if (process.env.NODE_ENV === "production") {
-  const paths = path.join(__dirname, "/frontend/mystore/dist");
-  console.log(paths);
-  app.use(express.static(paths));
+  app.use(express.static(path.join(__dirname, "/frontend/mystore/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(paths, "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "frontend", "mystore", "dist", "index.html")
+    );
   });
 }
 
